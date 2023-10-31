8bc2a45026cb7c6f945d144888840a05611cef14
zxltvn <zxltvnrxsess@gmail.com>
2023-10-31 15:42:16 +0300

    test1

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Montserrat', 'sans-serif'],
+        extra: ['Playfair Display', 'serif']
+      },
+      colors: {
+        tiffany: '#0ABAB5',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
